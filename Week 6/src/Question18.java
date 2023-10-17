fd3f394c4ac00ea50c3272fefd58e140363529df
YamahaNmax <m4mitzlearning@gmail.com>
2023-10-17 23:30:19 +0100

    0Pushing Homework

---
+++ Week 6/src/Question18.java
@@ -0,0 +1,35 @@
+public class Question18 {
+        public static void main(String[] args) {
+
+            // Personal details
+            String studentName = "Crime Master Gogo";
+            String programName = "Masters in Meme";
+            String institutionName = "GIF University";
+
+
+            // Subject marks
+            int lol = 95;
+            int rofl = 90;
+            int lmao = 85;
+            int sad = 38;
+            int humor = 89;
+
+            // Print the marksheet to the console
+            System.out.println("===============================================");
+            System.out.println("                   MARKSHEET");
+            System.out.println("===============================================");
+            System.out.println("Name:                         "+ studentName);
+            System.out.println("Program:                      "+ programName);
+            System.out.println("Institution:                  "+ institutionName);
+            System.out.println("-----------------------------------------------");
+            System.out.println("Subject                          Marks");
+            System.out.println("-----------------------------------------------");
+            System.out.println("Programming                      " + lol);
+            System.out.println("Data Structures                  " + rofl);
+            System.out.println("Databases                        " + lmao);
+            System.out.println("Computer Networks                " + sad);
+            System.out.println("Intelligence                     " + humor);
+            System.out.println("-----------------------------------------------");
+            System.out.println("===============================================");
+        }
+}
--- Week 6/src/Question20.java
+++ Week 6/src/Question20.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Question20 {
+public class Question20 {
+        public static void main(String[] args) {
+            int num1 = 5;
+            double num2 = 6.2;
+
+            System.out.println(num1 + "  and  " + num2);
+        }
 }
